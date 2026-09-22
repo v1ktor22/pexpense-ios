01789c9a4de03bc3f8877d1e978b5e893aa2c26f
Victor <vicawnolasco@gmail.com>
2026-09-22 18:46:02 +0200

    Initial Commit

---
+++ Pexpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pexpense
+//
+//  Created by Silva on 22.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pexpense/PexpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexpenseApp.swift
+//  Pexpense
+//
+//  Created by Silva on 22.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct PexpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
